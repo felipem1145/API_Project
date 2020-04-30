0165fde7d6db53d81e87d0b96b84957e0bf04c6d
felipem1145 <felipem1145@users.noreply.github.com>
2020-04-30 19:43:02 -0400

    WebService

---
+++ ToDo_WebService/ToDoApp_WS/ToDoApp_WS/App_Code/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Tasks
+/// </summary>
+public class Tasks
+{
+    public Tasks()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    public int TaskId;
+    public string Title;
+    public DateTime DueDate;
+    public string Place;
+    public string Category;
+    public string Description;
+    public Boolean TaskDone;
+    }
+}
